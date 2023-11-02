4a79d35b63f091851ee4f1b796aa9f83e9a42b25
haiundwo <146145092+haiundwo@users.noreply.github.com>
2023-11-02 09:57:52 +0800

    fix the shitty spacing

--- main.cpp
+++ main.cpp
@@ -2,8 +2,8 @@
 #include "GUI.h"
 
 int main(int, char**){
-  std::unique_ptr<GUI> gui = std::make_unique<GUI>();
-  gui->setBackgroundColor( 0.0f, 0.4f, 0.6f );
-  gui->mainLoop();
-  exit(EXIT_SUCCESS);
+	std::unique_ptr<GUI> gui = std::make_unique<GUI>();
+	gui->setBackgroundColor( 0.0f, 0.4f, 0.6f );
+	gui->mainLoop();
+	exit(EXIT_SUCCESS);
 }
